6204e6d189e7b73faa98ba217e53b6456e9cb6a5
Ben Simms <ben@bensimms.moe>
2018-10-16 15:11:33 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Whois.Mixfile do
   def project do
     [
       app: :whois,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.2",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
